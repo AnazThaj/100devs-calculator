9a138f7a3cf792a057298cb5639e183a3800cec8
Thaj Anaz <anaz.thaj@gmail.com>
2022-09-07 19:43:38 +0530

    Add eventlistener and a function

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-window.addEventListener('keydown', e => console.log(e.key))
+window.addEventListener('keydown', e => addTransition(e.key))
+
+function addTransition(pressedKey) {
+    const key = document.querySelector(`.key[key="${pressedKey}"]`);
+    const fkey = document.querySelector(`.key[key="${pressedKey}"]`);
+    const display = document.querySelector('.display')
+
+
+    if(!key || !fkey) return;
+
+    display.innerText = `${pressedKey}`
+}
