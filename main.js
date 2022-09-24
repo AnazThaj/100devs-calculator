d90fb928f7d3358a66ba8f0b43f3499874847d62
Thaj Anaz <anaz.thaj@gmail.com>
2022-09-25 02:50:33 +0530

    Format code

--- main.js
+++ main.js
@@ -1,39 +1,39 @@
 // TODO Display the pressed keys on the calculator display
-    // TODO Digits should be from Right to Left
-    // TODO Accept only numeric values
-    // TODO Choose the right font, size, and color for the display
-    // TODO Character limits (10/12)
-    // TODO Decimal point can only entered only once
-    // TODO Default display should be with '0'
+// TODO Digits should be from Right to Left
+// TODO Accept only numeric values
+// TODO Choose the right font, size, and color for the display
+// TODO Character limits (10/12)
+// TODO Decimal point can only entered only once
+// TODO Default display should be with '0'
 // TODO Calculation - '+' / '-' / '/' / 'x' / '='
 // TODO Function keys
-    // TODO On/Off toggle
-    // TODO When "AC" is pressed the display should be cleared with '0' appearing
+// TODO On/Off toggle
+// TODO When "AC" is pressed the display should be cleared with '0' appearing
 // *
 // *
 
 window.addEventListener("keydown", (e) => {
     addTransition(e.key);
-    displayInput(e.key)
+    displayInput(e.key);
 });
 
-window.addEventListener('click', (e) => {
+window.addEventListener("click", (e) => {
     addTransition(e.key);
-    displayInput(e.key)
-})
+    displayInput(e.key);
+});
 
 function addTransition(pressedKey) {
-    const keyPressed = document.querySelector(`.keys[key="${pressedKey}"]`)
-    if(!keyPressed) return
-    keyPressed.classList.add('pressed')
-    keyPressed.addEventListener('transitionend', resetTransition)
+    const keyPressed = document.querySelector(`.keys[key="${pressedKey}"]`);
+    if (!keyPressed) return;
+    keyPressed.classList.add("pressed");
+    keyPressed.addEventListener("transitionend", resetTransition);
 }
 
 function resetTransition(e) {
-    if(e.propertyName !=='transform') return
-    this.classList.remove('pressed')
+    if (e.propertyName !== "transform") return;
+    this.classList.remove("pressed");
 }
 
 function displayInput(pressedKey) {
-    const keyPressed = document.querySelector(`.keys[key="${pressedKey}"]`)
+    const keyPressed = document.querySelector(`.keys[key="${pressedKey}"]`);
 }
