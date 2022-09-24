a1f96532dd45e26705a862ea921138b409d15133
Thaj Anaz <anaz.thaj@gmail.com>
2022-09-24 23:23:29 +0530

    Add todo tasks as comments

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+// TODO Display the pressed keys on the calculator display
+    // TODO Digits should be from Right to Left
+    // TODO Accept only numeric values
+    // TODO Choose the right font, size, and color for the display
+    // TODO Character limits (10/12)
+    // TODO Decimal point can only entered only once
+    // TODO Default display should be with '0'
+// TODO Calculation - '+' / '-' / '/' / 'x' / '='
+// TODO Function keys
+    // TODO On/Off toggle
+    // TODO When "AC" is pressed the display should be cleared with '0' appearing
+// *
+// *
+
 window.addEventListener("keydown", (e) => {
     addTransition(e.key);
     displayInput(e.key)
