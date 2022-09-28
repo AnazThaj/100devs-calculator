5ceb8e1f664242449d31eb7f938fa0e653b05898
Thaj Anaz <anaz.thaj@gmail.com>
2022-09-28 19:20:06 +0530

    Restructure the directories - projects

--- main.js
+++ main.js
@@ -1,11 +1,83 @@
+// !Main function
 // TODO Display the pressed keys on the calculator display
 // TODO Digits should be from Right to Left
 // TODO Accept only numeric values
-// TODO Choose the right font, size, and color for the display
+// TODO Calculation - '+' / '-' / '/' / 'x' / '='
 // TODO Character limits (10/12)
 // TODO Decimal point can only entered only once
+
+// !Optional - designs, etc
+// TODO Choose the right font, size, and color for the display
 // TODO Default display should be with '0'
-// TODO Calculation - '+' / '-' / '/' / 'x' / '='
 // TODO Function keys
 // TODO On/Off toggle
-// TODO When "AC" is pressed the display should be cleared with '0' appearing
+// TODO When "AC" (all clear) is pressed the display should be cleared with '0' appearing
+
+const keys = document.querySelectorAll(".keys");
+keys.forEach(
+    (item) =>
+        //^Event listener for "clicks"
+        item.addEventListener("click", (e) => {
+            const keyValue = e.target.getAttribute("key");
+            calculator.parseInput(keyValue);
+        })
+    // TODO Add event listener for "keypresses"
+);
+
+const calculator = {
+    //* Properties
+    displayText: "0",
+    //^ Store the total from the previous operations
+    prevTotal: null,
+
+    //*Methods
+    parseInput(value) {
+        switch (value) {
+            case "o":
+            //Switch on the calculator and display "Off"
+            case "Escape":
+                // All clear
+                break;
+            case "Enter":
+                //Perform the calculations entered so far
+                break;
+            case "/":
+                //Divide
+                break;
+            case "x":
+                //Multiply
+                break;
+            case ".":
+                if (this.displayText === "0") {
+                    //Pass '0.' into text method
+                } else {
+                    //Add value to the text string
+                }
+                break;
+            case "-":
+                //Subtract
+                break;
+            default:
+                //Add value to the text string
+                break;
+        }
+    },
+    addText(value) {
+        //^ Getting rid of the zero before starting to display the character displayed
+        if (this.displayText === "0") {
+            this.displayText = "";
+        } else if (this.prevTotal) {
+            //Adding text to our input
+            this.displayText = this.prevTotal;
+            this.prevTotal = null;
+        }
+
+        if(true) {
+           //invalid sequence of operations
+           //Condition: last char in display AND the entered value are not numbers
+        }
+
+        this.displayText += value
+        //Output the result to the screen
+    },
+};
