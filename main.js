744558cbe533c0a811a3365e51d795f25000f1ab
Thaj Anaz <anaz.thaj@gmail.com>
2022-10-06 04:52:51 +0530

    Minor changes and basic adjustments

--- main.js
+++ main.js
@@ -15,79 +15,79 @@
 
 const keys = document.querySelectorAll(".keys");
 keys.forEach(
-    (item) =>
-        //^Event listener for "clicks"
-        item.addEventListener("click", (e) => {
-            const keyValue = e.target.getAttribute("key");
-            calculator.parseInput(keyValue);
-        })
-    // TODO Add event listener for "keypresses"
+  (item) =>
+    //^Event listener for "clicks"
+    item.addEventListener("click", (e) => {
+      const keyValue = e.target.getAttribute("key");
+      calculator.parseInput(keyValue);
+    })
+  // TODO Add event listener for "keypresses"
 );
 
 const calculator = {
-    //* Properties
-    displayText: "0",
-    //^ Store the total from the previous operations
-    prevTotal: null,
+  //* Properties
+  displayText: "0",
+  //^ Store the total from the previous operations
+  prevTotal: null,
 
-    //*Methods
-    parseInput(value) {
-        switch (value) {
-            case "o":
-            //Switch on the calculator and display "Off"
-            case "Escape":
-                // All clear
-                break;
-            case "Enter":
-                //Perform the calculations entered so far
-                break;
-            case "/":
-                //Divide
-                break;
-            case "x":
-                //Multiply
-                break;
-            case ".":
-                if (this.displayText === "0") {
-                    this.addText("0.");
-                } else {
-                    this.addText(value);
-                }
-                break;
-            case "-":
-                //Subtract
-                break;
-            default:
-                this.addText(value);
-                break;
-        }
-    },
-    addText(value) {
-        //^ Getting rid of the zero before starting to display the character displayed
+  //*Methods
+  parseInput(value) {
+    switch (value) {
+      case "o":
+      //Switch on the calculator and display "Off"
+      case "Escape":
+        // All clear
+        break;
+      case "Enter":
+        //Perform the calculations entered so far
+        break;
+      case "/":
+        //Divide
+        break;
+      case "x":
+        //Multiply
+        break;
+      case ".":
         if (this.displayText === "0") {
-            this.displayText = "";
-        } else if (this.prevTotal) {
-            //Adding text to our input
-            this.displayText = this.prevTotal;
-            this.prevTotal = null;
+          this.addText("0.");
+        } else {
+          this.addText(value);
         }
-
-        if (isNaN(+value) && isNaN(+this.displayText)) {
-            if (isNaN(this.displayText.slice(-1))) {
-                return;
-            }
-            //invalid sequence of operations
-            //Condition: last char in display AND the entered value are not numbers
-            //Such as either other operation signs +, -, x, etc
-        }
-        // Adding this comment to check the contribution status on Github. It's been acting up strangely
-        this.displayText += value;
-        this.outputText(this.displayText)
-    },
-    outputText(text) {
-        document.querySelector('.screen').value = text
-    },
-    calcAnswer(equation) {
-        let result = eval(equation)
+        break;
+      case "-":
+        //Subtract
+        break;
+      default:
+        this.addText(value);
+        break;
     }
+  },
+  addText(value) {
+    //^ Getting rid of the zero before starting to display the character displayed
+    if (this.displayText === "0") {
+      this.displayText = "";
+    } else if (this.prevTotal) {
+      //Adding text to our input
+      this.displayText = this.prevTotal;
+      this.prevTotal = null;
+    }
+
+    if (isNaN(+value) && isNaN(+this.displayText)) {
+      if (isNaN(this.displayText.slice(-1))) {
+        return;
+      }
+      //invalid sequence of operations
+      //Condition: last char in display AND the entered value are not numbers
+      //Such as either other operation signs +, -, x, etc
+    }
+    // Adding this comment to check the contribution status on Github. It's been acting up strangely
+    this.displayText += value;
+    this.outputText(this.displayText);
+  },
+  outputText(text) {
+    document.querySelector(".screen").value = text;
+  },
+  calcAnswer(equation) {
+    let result = eval(equation);
+  },
 };
